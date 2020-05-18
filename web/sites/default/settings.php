ccb3d4409ede67d486d2e3f6b8f0f51b9c6d4cb7
Bradley Waye <bradleywaye@Bradleys-MBP-2.hsd1.ga.comcast.net>
2020-05-18 16:52:18 -0400

    settings update

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -766,14 +766,4 @@ $databases['default']['default'] = array(
   'driver' => 'mysql',
 );
 
-/**
- * Load local development override configuration, if available.
- *
- * Use settings.local.php to override variables on secondary (staging,
- * development, etc) installations of this site. Typically used to disable
- * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
- * other things that should not happen on development and testing sites.
- *
- * Keep this code block at the end of this file to take full effect.
- */
 
