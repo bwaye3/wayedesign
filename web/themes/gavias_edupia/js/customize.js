fec3c7d766d7173d43d157faac2aa08933ba63c5
Brad Waye <bradwaye@gmail.com>
2023-01-24 09:36:08 -0500

    upgrade to php8

--- web/themes/gavias_edupia/js/customize.js
+++ web/themes/gavias_edupia/js/customize.js
@@ -30,7 +30,6 @@
   });
 
   $(window).load(function(){
-
     $('input.color-picker').each(function(){
       $color = $(this).val();
       $name = $(this).attr('name');
@@ -39,40 +38,16 @@
   })
 
   $(document).ready(function(){
-    function getByClass(sClass){
-      var aResult=[];
-      var aEle=document.getElementsByTagName('*');
-      for(var i=0;i<aEle.length;i++){
-        /*foreach className*/
-        var arr=aEle[i].className.split(/\s+/);
-        for(var j=0;j<arr.length;j++){
-          /*check class*/
-          if(arr[j]==sClass){
-            aResult.push(aEle[i]);
-          }
-        }
-      }
-      return aResult;
-    };
-
-
-    function runRender(type){
-      var aBox = getByClass("code_"+type);
-      for(var i=0;i < aBox.length; i++){
-        var editor = false;
-        if(!editor){
-          editor = CodeMirror.fromTextArea(aBox[i], {
-            lineNumbers: true,
-            mode: "text/css",
-          });
-        }
-        editor.on("blur", function() {editor.save()});
-      }
-    };
-
-    runRender('css');
+    var initCodeMirror = false;
+    var myTextarea = $('#edit-customize-css');
     $('a[href="#edit-css-customize"]').click(function(){
-      runRender('css');
+      if(!initCodeMirror){
+        var editor = CodeMirror.fromTextArea(document.getElementById("edit-customize-css"), {
+          lineNumbers: true,
+          mode: "text/css",
+        });
+      }
+      initCodeMirror = true;
     })
   })
 
